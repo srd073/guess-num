14c8b06c77eceafae41c8d09c119791a3ae6983d
srd073 <srd073@gmail.com>
2025-09-16 00:44:18 +0000

    final

--- guessnum.py
+++ guessnum.py
@@ -1,2 +1,21 @@
 #guess number using random
-print('aaa')
+import random
+s=1
+e=100
+x=random.randint(1,100)
+while True:
+    i=input('請輸入一個'+str(s)+'-'+str(e)+'的數字，猜一下電腦所產生的數字：')
+    i = int(i)
+    if i==x:
+        print('正確答案：',x,'你太厲害了，竟然知道電腦的答案')
+        break
+    if i>x:
+        print('電腦的數字小於',i)
+        e=i
+        
+    else:
+        print('電腦的數字大於',i)
+        s=i
+  
+    print(x)
+
